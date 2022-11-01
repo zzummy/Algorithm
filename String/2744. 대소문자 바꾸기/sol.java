65adf5bfb9d9825572c8f8070b7dc82746a6ac37
박주미 <zummy@kakao.com>
2022-11-01 16:28:46 +0900

    ✍🏻 Solve BOJ_2744_대소문자 바꾸기

---
+++ String/2744. 대소문자 바꾸기/sol.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sol {
+  public static void main(String[] args) throws IOException{
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+
+    String answer = "";
+
+    for (char c : str.toCharArray()) {
+      if (Character.isLowerCase(c))
+        answer += Character.toUpperCase(c);
+      else
+        answer += Character.toLowerCase(c);
+    }
+    
+    System.out.println(answer);
+  }
+  
+}
