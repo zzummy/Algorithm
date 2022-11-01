26710de1d37205b2990e9cbbaad85424861c5c32
박주미 <zummy@kakao.com>
2022-11-01 13:00:23 +0900

    ✍🏻 Solve BOJ_11365_!밀비 급일

---
+++ String/11365. !빌미 급일/sol.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sol {
+  public static void main(String[] args) throws IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    while (true) {
+      String str = br.readLine();
+      if (str.equals("END"))
+        break;
+
+      StringBuilder reverse = new StringBuilder(str);
+      reverse = reverse.reverse();
+      sb.append(reverse + "\n");
+    }
+    System.out.println(sb);
+  }
+}
