5c68f2be2ccd521f48b95bf2e04b6b137480de93
박주미 <zummy@kakao.com>
2022-11-03 04:12:38 +0900

    ✍🏻 Solve BOJ_6996_애너그램

---
+++ String/6996. 애너그램/README.md
@@ -0,0 +1,19 @@
+## 6996. 애너그램
+
+​           
+
+입력 받은 문자열들을 char형 배열로 변환한 후, 문자 하나 씩 비교
+
+`str.toCharArray()` 
+
+- 문자열을 char[]으로 변환
+
+`Arrays.sort(arr)` 
+
+- 오름차순으로 배열 정렬
+
+`Arrays.equals(arr1, arr2)`
+
+- 두 개의 배열 비교
+- true/false
+
---
+++ String/6996. 애너그램/애너그램.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 애너그램 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    for (int i = 0; i < N; i++) {
+      String firstStr = sc.next();
+      String secondStr = sc.next();
+      isAnagram(firstStr, secondStr);
+    }
+    sc.close();
+  }
+
+  public static void isAnagram(String str1, String str2) {
+    char[] cArr1 = str1.toCharArray();
+    char[] cArr2 = str2.toCharArray();
+
+    Arrays.sort(cArr1);
+    Arrays.sort(cArr2);
+
+    // System.out.println(cArr1);
+
+    if(Arrays.equals(cArr1, cArr2))
+      System.out.println(str1 + " & " + str2 + " are anagrams.");
+    else System.out.println(str1 + " & " + str2 + " are NOT anagrams.");
+  }
+}
