ebce1579fa25b4383ab01a3dfde616019f5c29a8
박주미 <zummy@kakao.com>
2022-11-10 03:53:14 +0900

    ✍🏻 Solve BOJ_20154_이 구역의 승자는 누구야

---
+++ String/20154. 이 구역의 승자는 누구야/승자는누구야.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 승자는누구야 {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] alphabet = { 3, 2, 1, 2, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 1 };
+    
+    String input = br.readLine();
+    int[] arr = new int[input.length()];
+    
+    for (int i = 0; i < arr.length; i++) {
+      // A이면 A - A = 0 이니까 3이 들어감
+      arr[i] = alphabet[input.charAt(i) - 'A'];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      sum %= 10;
+    }
+
+    if (sum % 2 == 0)
+      System.out.println("You're the winner?");
+    else
+      System.out.println("I'm a winner!");
+    
+  }
+}
