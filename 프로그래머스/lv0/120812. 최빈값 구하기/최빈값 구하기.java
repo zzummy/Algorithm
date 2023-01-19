5496acadd84544c0ff844fa45d49b3fb906c484a
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-01-20 04:19:49 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.11 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        int[] index = new int[2001];
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0; i<array.length; i++){
+            index[array[i]]++;
+        }
+        
+        for(int i=0; i<index.length; i++){
+            if(index[i] > max){
+                max = index[i];
+                answer = i;
+            }else if(index[i] == max) answer = -1;
+            else continue;
+        }
+        
+        
+        
+        return answer;
+    }
+}
