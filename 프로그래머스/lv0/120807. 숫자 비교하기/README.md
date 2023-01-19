6d9a575179e2fa30fd5d03cbe36b02cee32cff1c
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-01-19 14:29:40 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/README.md
+++ 프로그래머스/lv0/120807. 숫자 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숫자 비교하기 - 120807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120807) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=java) 
 
 ### 성능 요약
 
-메모리: 69.2 MB, 시간: 0.02 ms
+메모리: 77.7 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        
-        if(num1 == num2) answer = 1;
-        else answer = -1;
-        
-        return answer;
+        int answer = 0;        
+        return answer = (num1==num2 ? 1 : -1);
     }
 }
