991c399ec50aef9f62f90c14b1b2caf6703301a6
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-01-23 13:00:38 +0900

    [level 0] Title: 문자열 뒤집기, Time: 11.28 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i=0; i<my_string.length(); i++){
+            answer += my_string.charAt(my_string.length() - 1 - i);
+        }
+        
+        return answer;
+    }
+}
