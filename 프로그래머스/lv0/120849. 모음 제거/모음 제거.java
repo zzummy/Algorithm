66ad60cdd032d41d7aca6296da57831bb0a89ee2
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-01-27 02:17:56 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -4,18 +4,14 @@ class Solution {
     public String solution(String my_string) {
         StringBuilder answer = new StringBuilder();
         
-        // String[] ms = new String[my_string.length()];
-        // for(int i = 0; i < my_string.length(); i++)
-        //     ms[i] = my_string.charAt(i);
         
         // 모음 배열 선언
-        String[] vowel = {"a", "e", "i", "o", "u"};
+        char[] vowel = {'a', 'e', 'i', 'o', 'u'};
         
         for(int i = 0; i < my_string.length(); i++){
             boolean not_vowl = true;
             for(int j = 0; j < vowel.length; j++){
-                
-                if(vowel[j].equals(Character.toString(my_string.charAt(i)))) {
+                 if(vowel[j] == my_string.charAt(i)) {
                     not_vowl = false;
                 }
             }
