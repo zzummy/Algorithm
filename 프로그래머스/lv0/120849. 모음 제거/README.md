480127959412dc1d25d2d74ae3cc779e356405b5
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-01-27 02:31:38 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/README.md
+++ 프로그래머스/lv0/120849. 모음 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모음 제거 - 120849 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849?language=java) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 0.05 ms
+메모리: 75.8 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,24 +1,12 @@
-import java.util.*;
-
 class Solution {
     public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
+        String[] vowels = {"a", "e", "i", "o", "u"};
         
-        
-        // 모음 배열 선언
-        char[] vowel = {'a', 'e', 'i', 'o', 'u'};
-        
-        for(int i = 0; i < my_string.length(); i++){
-            boolean not_vowl = true;
-            for(int j = 0; j < vowel.length; j++){
-                 if(vowel[j] == my_string.charAt(i)) {
-                    not_vowl = false;
-                }
-            }
-            if(not_vowl)
-                answer.append(my_string.charAt(i));
+        for(String v : vowels){
+            if(my_string.contains(v))
+                my_string = my_string.replace(v ,"");
         }
         
-        return answer.toString();
+        return my_string;
     }
 }
