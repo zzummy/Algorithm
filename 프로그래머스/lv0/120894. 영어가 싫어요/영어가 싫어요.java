fc567e8c988f8e16ede7fc11818cb72b30e37626
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-02-01 01:54:02 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.13 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(String numbers) {
+        String str = numbers
+            .replace("one", "1")
+            .replace("two","2")
+            .replace("three","3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9")
+            .replace("zero", "0");
+        
+        long answer = Long.parseLong(str);
+        
+        return answer;
+    }
+}
