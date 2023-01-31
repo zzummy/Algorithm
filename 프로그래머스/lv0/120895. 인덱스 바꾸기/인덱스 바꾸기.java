01f01c8d9c56528cbc730cc672042449c4ed3687
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-02-01 01:49:56 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.26 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        
+        char tmp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = tmp;
+        
+        for(char c : arr){
+            answer += Character.toString(c);
+        }
+        
+        return answer;
+    }
+}
