daa12528e6952378ef2c50cc92c2f7f8082850bf
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-02-07 02:54:38 +0900

    [level 0] Title: 평행, Time: 0.33 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,25 +1,6 @@
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        float[] arr = new float[6];
-        
-        int idx = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = i+1; j < 4; j++){
-                arr[idx++] = (float)(dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]);
-            }
-        }
-        
-        for(int i = 0; i < 5; i++){
-            for(int j = i+1; j < 6; j++){
-                String s1 = String.valueOf(arr[i]);
-                String s2 = String.valueOf(arr[j]);
-                if(s1.equals(s2)){
-                    return 1;
-                }
-            }
-        }
-        
         return answer;
     }
 }
