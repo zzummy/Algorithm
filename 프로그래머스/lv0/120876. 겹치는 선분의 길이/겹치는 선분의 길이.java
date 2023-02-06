857c9d68734965d8f587606893dbf78788ef0420
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-02-07 03:09:38 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,19 +1,6 @@
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        int[] arr = new int[201];
-        
-        // linew는 -100 ~ 100이므로 +100씩 하면 0 ~ 200이 되어 수월함
-        for(int i =0; i < lines.length; i++){
-            for(int j = lines[i][0] + 100; j < lines[i][1] + 100; j++){
-                arr[j]++;
-            }
-        }
-        
-        for(int n : arr){
-            // 1이상이면 겹친 부분이므로
-            if(n > 1) answer++;
-        }
         return answer;
     }
 }
