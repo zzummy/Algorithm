7698b04ccd7eb740cd063647c5b092968547c092
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-03-14 22:00:02 +0900

    [Gold V] Title: 탑, Time: 3556 ms, Memory: 126724 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,30 +5,30 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    Stack<int[]> stack = new Stack<>();
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(bf.readLine());
-		
-		String s = bf.readLine();
-		StringTokenizer st = new StringTokenizer(s, " ");
-		Stack<int[] > stack = new Stack<>();
-		
-		for (int i = 1; i <= N ; i++) {
-			int num = Integer.parseInt(st.nextToken());
+    for (int n = 1; n <= N; n++) {
+      int len = Integer.parseInt(st.nextToken());
 
-			while(!stack.isEmpty()) {
-				if(stack.peek()[1] > num) {
-					System.out.print(stack.peek()[0] +" ");
-					break;
-				}else if(stack.peek()[1] < num)
-					stack.pop();
-			}
-			if(stack.isEmpty()) {
-				System.out.print(0 + " ");
-			}
-			stack.push(new int[] {i,num});
-		}
-	}
+      while (!stack.isEmpty()) {
+        if (stack.peek()[1] > len) {
+          System.out.print(stack.peek()[0] + " ");
+          break;
+        } else {
+          stack.pop();
+        }
+      }
+
+      if (stack.isEmpty()) {
+        System.out.print(0 + " ");
+      }
+
+      // [ 탑 번호, 길이 ]
+      stack.push(new int[] { n, len });
+    }
+  }
 }
