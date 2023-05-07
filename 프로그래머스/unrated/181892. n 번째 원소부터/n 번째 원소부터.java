86eca390f9f16f1c9a9b7f96d66b88525bc7398b
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-05-08 00:24:35 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length - n + 1];
+        
+        int idx = 0;
+        for(int i = n; i <= num_list.length; i++){
+            answer[idx++] = num_list[i - 1];
+        }
+            
+        
+        return answer;
+    }
+}
