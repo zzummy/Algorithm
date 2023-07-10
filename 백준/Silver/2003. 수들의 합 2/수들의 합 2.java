4890f5fe3b47dba6fd4a7cf54468df5f651fc94a
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-07-11 03:56:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -3,40 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// https://www.acmicpc.net/problem/2003
+
 public class Main {
-  public static int twoPointer(int[] arr, int N, int M) {
-    int answer = 0, sum = 0;
-    int s = 0, e = 0; // s <= e
-
-    while (s < N) {
-      // 연속된 합sum이 M보다 크거나 같으면, 맨 왼쪽 값(arr[s]) 빼주고 s++
-      if (sum >= M)
-        sum -= arr[s++];
-      // 맨 끝에 도착했으면 종료
-      else if (e == N)
-        break;
-      // sum < M
-      else
-        sum += arr[e++];
-
-      if (sum == M)
-        answer++;
-    }
-
-    return answer;
-  }
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
+
     int[] arr = new int[N];
+
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    System.out.println(twoPointer(arr, N, M));
+    int sum = 0, answer = 0;
+    int lt = 0, rt = 0;
+    while (true) {
+      if (sum >= M)
+        sum -= arr[lt++];
+      else if (rt >= N)
+        break;
+      else
+        sum += arr[rt++];
+
+      if (sum == M)
+        answer++;
+    }
+
+    System.out.println(answer);
   }
 }
