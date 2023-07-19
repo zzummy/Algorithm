f158737445b1dfff31e7ef342d4fe5c110ce4004
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-07-20 03:26:02 +0900

    [Silver IV] Title: 수 찾기, Time: 576 ms, Memory: 48804 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,53 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
   static int N, M;
-  static int[] arr;
+  static HashSet<Integer> set = new HashSet<>();
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     N = Integer.parseInt(br.readLine());
-    arr = new int[N];
-
     StringTokenizer st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
+      set.add(Integer.parseInt(st.nextToken()));
     }
-
-    Arrays.sort(arr);
 
     int M = Integer.parseInt(br.readLine());
     st = new StringTokenizer(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
     for (int i = 0; i < M; i++) {
       int num = Integer.parseInt(st.nextToken());
 
-      if (binarySearch(num))
-        System.out.println(1);
+      if (set.contains(num))
+        sb.append(1).append("\n");
       else
-        System.out.println(0);
+        sb.append(0).append("\n");
     }
 
-  }
-
-  public static boolean binarySearch(int num) {
-    int lt = 0, rt = N - 1;
-
-    while (lt <= rt) {
-      int mid = (lt + rt) / 2;
-
-      if (arr[mid] > num)
-        rt = mid - 1;
-      else if (arr[mid] < num)
-        lt = mid + 1;
-      else
-        return true;
-    }
-
-    return false;
+    System.out.println(sb);
   }
 }
