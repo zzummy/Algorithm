f231604ea7ea8f1174726678296206bc10d0712c
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-07-25 21:23:46 +0900

    [Silver I] Title: 영역 구하기, Time: 136 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -6,24 +6,11 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int M, N, K, cnt = 0;
-  static int[][] maps;
-  static int[] dx = { 0, 1, 0, -1 };
-  static int[] dy = { 1, 0, -1, 0 };
-  static ArrayList<Integer> list = new ArrayList<>();
-
-  public static void DFS(int x, int y) {
-    maps[x][y] = 1;
-    cnt++;
-
-    for (int d = 0; d < 4; d++) {
-      int nx = x + dx[d];
-      int ny = y + dy[d];
-      if (nx >= 0 && nx < M && ny >= 0 && ny < N && maps[nx][ny] == 0) {
-        DFS(nx, ny);
-      }
-    }
-  }
+  static int M, N, K;
+  static int[][] map;
+  static int count;
+  static int[] dx = { 0, 0, -1, 1 };
+  static int[] dy = { 1, -1, 0, 0 };
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,7 +18,7 @@ public class Main {
     M = Integer.parseInt(st.nextToken());
     N = Integer.parseInt(st.nextToken());
     K = Integer.parseInt(st.nextToken());
-    maps = new int[M][N];
+    map = new int[M][N];
 
     for (int k = 0; k < K; k++) {
       st = new StringTokenizer(br.readLine());
@@ -42,25 +29,44 @@ public class Main {
 
       for (int x = y1; x < y2; x++) {
         for (int y = x1; y < x2; y++) {
-          maps[x][y] = 1;
+          map[x][y] = 1;
         }
       }
     }
 
+    ArrayList<Integer> list = new ArrayList<>();
     for (int i = 0; i < M; i++) {
       for (int j = 0; j < N; j++) {
-        if (maps[i][j] == 0) {
-          cnt = 0;
+        if (map[i][j] == 0) {
+          count = 0;
           DFS(i, j);
-          list.add(cnt);
+          list.add(count);
         }
       }
     }
 
-    System.out.println(list.size());
     Collections.sort(list);
-    for (int n : list)
-      System.out.print(n + " ");
 
+    System.out.println(list.size());
+    for (Integer num : list)
+      System.out.print(num + " ");
+  }
+
+  public static void DFS(int x, int y) {
+    map[x][y] = 1;
+    count++;
+
+    for (int d = 0; d < 4; d++) {
+      int nx = x + dx[d];
+      int ny = y + dy[d];
+
+      if (check(nx, ny) && map[nx][ny] == 0) {
+        DFS(nx, ny);
+      }
+    }
+  }
+
+  public static boolean check(int x, int y) {
+    return x >= 0 && x < M && y >= 0 && y < N;
   }
 }
