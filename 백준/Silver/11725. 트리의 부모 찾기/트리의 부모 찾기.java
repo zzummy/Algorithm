6f82369917651d36b988ef39a2d85ad49411625d
Z U M I <62216158+zzummy@users.noreply.github.com>
2023-08-07 03:23:28 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1728 ms, Memory: 78744 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int N;
+  static int[] parent; // 부모 노드 저장
+  static boolean[] visited;
+  static ArrayList<Integer> list[]; // 노드 저장
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    parent = new int[N + 1];
+    visited = new boolean[N + 1];
+    list = new ArrayList[N + 1];
+
+    for (int i = 0; i < N + 1; i++) {
+      list[i] = new ArrayList<>();
+    }
+
+    StringTokenizer st;
+    for (int i = 0; i < N - 1; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+
+      list[a].add(b);
+      list[b].add(a);
+    }
+
+    DFS(1);
+
+    for (int i = 2; i < parent.length; i++) {
+      System.out.println(parent[i]);
+    }
+  }
+
+  public static void DFS(int n) {
+    visited[n] = true;
+
+    for (int i : list[n]) {
+      if (!visited[i]) {
+        parent[i] = n;
+        DFS(i);
+      }
+    }
+  }
+}
